5f9050f870b199d40f995ee8920cc09bcd29476b
Matt Robenolt <matt@ydekproductions.com>
2025-04-01 22:03:48 -0700

    Add benchmarks for builtin collectors

---
+++ collectors_bench_test.go
@@ -0,0 +1,21 @@
+package metrics
+
+import "testing"
+
+func BenchmarkCollectors(b *testing.B) {
+	benchmarkCollector(b, "Go", NewGoMetricsCollector())
+	benchmarkCollector(b, "Process", NewProcessMetricsCollector())
+	benchmarkCollector(b, "Self", NewSelfMetricsCollector())
+}
+
+func benchmarkCollector(b *testing.B, name string, c Collector) {
+	b.Run(name, func(b *testing.B) {
+		w := NewTestingExpfmtWriter()
+		b.ReportAllocs()
+		for b.Loop() {
+			w.b.Reset()
+			c.Collect(w)
+		}
+
+	})
+}
